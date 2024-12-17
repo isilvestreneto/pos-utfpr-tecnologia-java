c7b8a7c6f3c46b2e11f4b7893269cc986232aca5
Neto <ivanildo.neto@capgemini.com>
2024-12-16 21:41:04 -0300

    java1 atividade 6

--- java1/IvanildoSilvestreDaSilvaNeto_Atividade06/src/main/java/org/example/VeicExistException.java
+++ java1/IvanildoSilvestreDaSilvaNeto_Atividade06/src/main/java/org/example/VeicExistException.java
@@ -5,20 +5,4 @@ public class VeicExistException extends Exception {
         public VeicExistException() {
             super("Veículo já cadastrado!");
         }
-
-        public VeicExistException(String msg) {
-            super(msg);
-        }
-
-        public VeicExistException(String msg, Throwable cause) {
-            super(msg, cause);
-        }
-
-        public VeicExistException(Throwable cause) {
-            super(cause);
-        }
-
-        public VeicExistException(String msg, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-            super(msg, cause, enableSuppression, writableStackTrace);
-        }
 }
